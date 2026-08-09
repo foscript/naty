9b7b2e8c0f83a09c41de1b2c370f8dbaa960b4b0
Foscript <foscript@gmail.com>
2026-08-09 06:57:42 +0000

    やっと機嫌直ったわ。カス

--- new-web/vite.config.ts
+++ new-web/vite.config.ts
@@ -3,18 +3,19 @@ import { defineConfig } from 'vite'
 // Plugins
 import { devtools } from '@tanstack/devtools-vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
-import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tanstackStart(),
-    viteReact(),
+    react(),
   ],
-
-  resolve: { tsconfigPaths: true }
 })
+
+export default config
